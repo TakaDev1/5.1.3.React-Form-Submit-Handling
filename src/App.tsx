826a35ae3eab26be54eb6c0ef6144cff1ae71ce3
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-25 18:53:26 +0900

    style: Project Adjustment Design

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CheckPage from "./pages/CheckPage";
 function App() {
   return (
     <>
-      <div>
+      <div className="min-h-screen bg-gray-800 flex flex-col justify-center">
         <h1>5.1.3.React-Form-Submit-Handling</h1>
         <CheckPage />
       </div>
